b71dd483c7d3550085ef2bb5757938a4ded3ca17
Swan0526 <102525538+Swan0526@users.noreply.github.com>
2024-05-17 17:46:34 +0200

    Sword cliping fix
    changement pour que l'épée passe dérriere le personnage quand elle est derierre logique

--- Assets/C#/Sword/SwordBehaviour.cs
+++ Assets/C#/Sword/SwordBehaviour.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class SwordBehaviour : MonoBehaviour
 {
     public float SweepSpeed = 10f;
+
+    private float Z_angle;
     Rigidbody2D rb;
     Vector2 MousePosition;
+    private SpriteRenderer sprite;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -24,7 +30,18 @@ public class SwordBehaviour : MonoBehaviour
             MousePosition.y - transform.position.y
         );
         transform.up = direction;
+
+        Z_angle = transform.eulerAngles.z;
         
+        if(Z_angle>270 || Z_angle<90){
+            sprite.sortingOrder = -1;
+        }
+        else{
+            sprite.sortingOrder = 1;
+        }
+
+        UnityEngine.Debug.Log(transform.eulerAngles.z);
+
         transform.Rotate (0,0,45);  // Parce que le sprite est orienté à -45° donc il faut compenser
     }
 }
