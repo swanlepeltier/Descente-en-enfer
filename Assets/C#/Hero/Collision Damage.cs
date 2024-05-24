e998fde8e8979c9c7bd5c81bb055d060f20f1a58
Swan0526 <102525538+Swan0526@users.noreply.github.com>
2024-05-24 17:58:23 +0200

    debug_log_remove

--- Assets/C#/Hero/Collision Damage.cs
+++ Assets/C#/Hero/Collision Damage.cs
@@ -22,7 +22,6 @@ public class CollisionDetection_hero : MonoBehaviour
             Hero_Health = (float)Variables.Object(this.gameObject).Get("Hero_Health") - (float)Variables.Object(coll.gameObject).Get("Damage");
             
             Variables.Object(this.gameObject).Set("Hero_Health",Hero_Health);
-            Debug.Log(Variables.Object(this.gameObject).Get("Hero_Health"));
         }
     }
 }
