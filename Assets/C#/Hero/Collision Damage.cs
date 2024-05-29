cc7c40d722eb4bbf4c05ea1f5c4321f2f301566a
Swan0526 <102525538+Swan0526@users.noreply.github.com>
2024-05-29 18:23:01 +0200

    Health_Bar
    Added Health_Bar and changing the size of the hero collider

--- Assets/C#/Hero/Collision Damage.cs
+++ Assets/C#/Hero/Collision Damage.cs
@@ -12,8 +12,11 @@ public class CollisionDetection_hero : MonoBehaviour
     
     private bool hero_invicible;
 
+    public HealthBar healthBar;
+
  void Start(){
         Hero_Health = (float)Variables.Object(this.gameObject).Get("Hero_Health");
+        healthBar.SetMaxHealth(Hero_Health);
     }
 
     // Fonction appelée lorsqu'une collision se produit
@@ -24,6 +27,7 @@ public class CollisionDetection_hero : MonoBehaviour
             if (hero_invicible == false){
                 Debug.Log("Héro Touché");
                 Hero_Health = (float)Variables.Object(this.gameObject).Get("Hero_Health") - (float)Variables.Object(coll.gameObject).Get("Damage");
+                healthBar.SetHealth(Hero_Health);
                 
                 Variables.Object(this.gameObject).Set("Hero_Health",Hero_Health);
                 if (Hero_Health <= 0){
